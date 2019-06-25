c0fe400639442b352050f347f3c642ea9318cacd
Wioletta Nowak <w.polecka19@wp.pl>
2019-06-25 12:55:28 +0200

    bem

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ const age='27';
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const mainHeader = document.querySelector('.mainHeader');
+console.log(mainHeader);
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+
+console.log(paragraphs[0]);
+
+const about = document.querySelector('.about');
+console.log(about.innerHTML);
+about.innerHTML = 'Pozdro JS';
